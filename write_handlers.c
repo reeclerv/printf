8ab5c3e1730584f06b91dd23a3256a07d57760e5
reeclerv <abutes@protonmail.com>
2023-07-25 14:57:44 -0700

    neg precision

--- write_handlers.c
+++ write_handlers.c
@@ -139,48 +139,23 @@ int display_unsgnd(int is_negative, int ind,
 	char buffer[],
 	int flags, int width, int precision, int size)
 {
-	/* The number is stored at the bufer's right and starts at position i */
-	int length = BUFF_SIZE - ind - 1, i = 0;
-	char padd = ' ';
+int length = BUFF_SIZE - ind - 1;
+	char padd = ' ', extra_ch = 0;
 
-	UNUSED(is_negative);
 	UNUSED(size);
-
-	if (precision == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
-		return (0); /* printf(".0d", 0)  no char is printed */
-
-	if (precision > 0 && precision < length)
-		padd = ' ';
-
-	while (precision > length)
-	{
-		buffer[--ind] = '0';
-		length++;
-	}
 
 	if ((flags & F_ZERO) && !(flags & F_NEG))
 		padd = '0';
+	if (is_negative)
+		extra_ch = '-';
+	else if (flags & F_POS)
+		extra_ch = '+';
+	else if (flags & F_SPACE)
+		extra_ch = ' ';
 
-	if (width > length)
-	{
-		for (i = 0; i < width - length; i++)
-			buffer[i] = padd;
-
-		buffer[i] = '\0';
-
-		if (flags & F_NEG) /* Asign extra char to left of buffer [buffer>padd]*/
-		{
-			return (write(1, &buffer[ind], length) + write(1, &buffer[0], i));
-		}
-		else /* Asign extra char to left of padding [padd>buffer]*/
-		{
-			return (write(1, &buffer[0], i) + write(1, &buffer[ind], length));
-		}
-	}
-
-	return (write(1, &buffer[ind], length));
+	return (display_num(ind, buffer, flags, width, precision,
+		length, padd, extra_ch));
 }
-
 
 /*************DISPLAY POINTER*****************************************/
 /**
