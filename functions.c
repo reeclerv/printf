4fe296e66728299770864fadcbb615becac93e95
Evalyne306 <wanjikueva306@gmail.com>
2023-07-25 11:14:50 -0700

    files executable

---
+++ functions.c
@@ -0,0 +1,178 @@
+#include "main.h"
+
+
+
+/*
+ * PRINT C
+ * handle_char is a function used to handle and print a single character
+ * takes the following parameters;
+ * va_list types, char buffer[], int flags, int width, int precision and 
+ * int size.
+ * it returns number of char printed
+ */
+int handle_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	char c = va_arg(types, int);
+
+	return (handle_write_char(c, buffer, flags, width, precision, size));
+}
+
+ /**
+ * PRINT S
+ * handle_string function used to handle the string
+ * the function takes several arguments including;
+ * va_list types, char buffer[], int flags, int width 
+ * and int size
+ */
+
+int handle_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int length = 0, i;
+	char *str = va_arg(types, char *);
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	if (str == NULL)
+	{
+		str = "(null)";
+		if (precision >= 6)
+			str = "      ";
+	}
+
+	while (str[length] != '\0')
+		length++;
+
+	if (precision >= 0 && precision < length)
+		length = precision;
+
+	if (width > length)
+	{
+		if (flags & F_MINUS)
+		{
+			write(1, &str[0], length);
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			return (width);
+		}
+		else
+		{
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			write(1, &str[0], length);
+			return (width);
+		}
+	}
+
+	return (write(1, str, length));
+}
+
+
+/**
+* PRINT %
+* handle_percent has been used to handle the percentages.
+* it takes in parameters; va_list types, char buffer[], 
+* int flags, int widt, int precision and int size
+* UNUSED macros which is used to suppress compiler warnings 
+* about unused function parameters.
+*/
+
+int handle_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	UNUSED(types);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	return (write(1, "%%", 1));
+}
+
+/**
+ * PRINT INT
+/**
+ * handle_int function prints int
+ * it takes in parameters; va_list types, char buffer[], 
+ * int flags, int widt, int precision and int size
+ */
+int handle_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int i = BUFF_SIZE - 2;
+	int is_negative = 0;
+	long int n = va_arg(types, long int);
+	unsigned long int num;
+
+	n = convert_size_number(n, size);
+
+	if (n == 0)
+		buffer[i--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+	num = (unsigned long int)n;
+
+	if (n < 0)
+	{
+		num = (unsigned long int)((-1) * n);
+		is_negative = 1;
+	}
+
+	while (num > 0)
+	{
+		buffer[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+
+	i++;
+
+	return (write_number(is_negative, i, buffer, flags, width, precision, size));
+}
+
+/**
+ * PRINT BINARY
+/**
+ * handle_binary function prints binary
+ * it takes in parameters; va_list types, char buffer[], 
+ * int flags, int widt, int precision and int size
+ */
+ n: Numbers of char printed.
+ 
+int handle_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	n = va_arg(types, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
+
+			write(1, &z, 1);
+			count++;
+		}
+	}
+	return (count);
+}
