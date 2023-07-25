2f5f655b77c0c6443fc0bb9bf9772ad408e5ea8a
reeclerv <101083725+reeclerv@users.noreply.github.com>
2023-07-25 22:07:55 +0300

    Update handlers

--- handle_print.c
+++ handle_print.c
@@ -7,7 +7,7 @@
  */
  * Return: 1 or 2;
  */
-int handle_printing(const char *fmt, int *ind, va_list list, char buffer[],
+int handle_printing(const char *fmt, int *index, va_list arglist, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
@@ -19,26 +19,26 @@ int handle_printing(const char *fmt, int *ind, va_list list, char buffer[],
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+		if (fmt[*index] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(arglist, buffer, flags, width, precision, size));
 
 	if (fmt_types[i].fmt == '\0')
 	{
-		if (fmt[*ind] == '\0')
+		if (fmt[*index] == '\0')
 			return (-1);
 		unknow_len += write(1, "%%", 1);
-		if (fmt[*ind - 1] == ' ')
+		if (fmt[*index - 1] == ' ')
 			unknow_len += write(1, " ", 1);
 		else if (width)
 		{
-			--(*ind);
-			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-				--(*ind);
-			if (fmt[*ind] == ' ')
-				--(*ind);
+			--(*index);
+			while (fmt[*index] != ' ' && fmt[*index] != '%')
+				--(*index);
+			if (fmt[*index] == ' ')
+				--(*index);
 			return (1);
 		}
-		unknow_len += write(1, &fmt[*ind], 1);
+		unknow_len += write(1, &fmt[*index], 1);
 		return (unknow_len);
 	}
 	return (printed_chars);
